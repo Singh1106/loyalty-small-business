76eb99192d300a0596e6454057ffc61e05b36377
Jaswinder Singh <jaswindersingh11061998@gmail.com>
2023-05-28 20:52:27 +0530

    chore: improved jwt interceptor

--- src/interceptors/verify-jwt-and-fetch-payload.ts
+++ src/interceptors/verify-jwt-and-fetch-payload.ts
@@ -3,7 +3,10 @@ import {
   NestInterceptor,
   ExecutionContext,
   CallHandler,
+  HttpException,
+  HttpStatus,
 } from '@nestjs/common';
+import to from 'await-to-js';
 import { JwtAuthService } from 'src/modules/jwt-auth/jwt-auth.service';
 
 @Injectable()
@@ -12,9 +15,18 @@ export class VerifyJWTAndFetchPayload implements NestInterceptor {
   async intercept(context: ExecutionContext, next: CallHandler) {
     const request = context.switchToHttp().getRequest();
     const authorization = request.headers.authorization;
-    const tokenPayload = await this.jwtAuthService.verifyJWT(
-      authorization.split(' ')[1],
+    if (!authorization) {
+      throw new HttpException(
+        'Please provide bearer token',
+        HttpStatus.UNAUTHORIZED,
+      );
+    }
+    const [error, tokenPayload] = await to(
+      this.jwtAuthService.verifyJWT(authorization.split(' ')[1]),
     );
+    if (error || !tokenPayload) {
+      throw new HttpException('Bearer token invalid', HttpStatus.UNAUTHORIZED);
+    }
     request.tokenPayload = tokenPayload;
     request.token = authorization.split(' ')[1];
     return next.handle();
